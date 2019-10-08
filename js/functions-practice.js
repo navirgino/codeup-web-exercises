54e6eb3d43bf0eeb27e3c513d981710bf93d7aee
Nico Virgino <nicovirgino@gmail.com>
2019-10-08 11:58:35 -0500

    functions exercise

---
+++ js/functions-practice.js
@@ -0,0 +1,17 @@
+function multiply(x, y){
+    return x * y;
+}
+console.log(multiply(3, 1));
+
+var divide = function(x, y) {
+    return (x / y);
+}
+
+console.log(divide(multiply(6, 2), 6));
+
+
+/*function multiplication (x= 3 , y= 2){
+    return x * y;
+}*/
+
+console.log(multiplication(5, 3));
