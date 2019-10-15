3bad1120bc4015d2bdc409ce38685cc2fd69c41d
Nico Virgino <nicovirgino@gmail.com>
2019-10-15 13:38:21 -0500

    while loops

--- js/while.js
+++ js/while.js
@@ -2,22 +2,14 @@
 
 //make a loop that multiplies by itself
 
-/*var i = 0;
-while (i<16) {
-    console.log(i);
-}*/
-// var n = 2;
-// var x = 0;
-// while(n<16){
-//     console.log(n++);
-//     console.log(n *= x++);
-// }
 
-var n = 0;
-var m = 0;
-while(n<17){
-    console.log(n++ * m++ * 2);
+/*
+var i = 2;
+while(i<65537){
+    console.log(i);
+    i *=2;
 }
+*/
 
 /*
 2
@@ -37,3 +29,50 @@ while(n<17){
 32768
 65536
 */
+
+// create a js program that generates a random number between 50 and 100
+// generate numbers between 1 and 5
+// This is how you get a random number between 50 and 100
+// amount of cones to sell
+// This expression will generate a random number between 1 and 5
+
+
+ // amount of cones to sell
+// generate code that generates numbers between 1 and 5, simulating the amount of cones
+// being bought by her clients.
+// log to the console the amount of cones sold to each person
+do {
+
+    var allCones = Math.floor(Math.random() * 50) + 50;
+
+    var conesRequested = Math.floor(Math.random() * 5) + 1; //request cone(s) per person
+
+    console.log(allCones + " cones available.");
+
+    if(allCones > conesRequested){
+
+        console.log("I sold " + conesRequested + " cones.");
+
+    }else if(conesRequested > allCones){
+        // if there are not enough cones
+        console.log("Cannot sell you " + conesRequested + " I only have " + allCones + " left." )
+
+    }else if (allCones === 0){
+        //once cones are sold out
+        console.log("Yay I sold them all!!!!!!!!!");
+
+    }
+
+    console.log(allCones - conesRequested + " cones remain.");
+    allCones++
+
+}while(allCones <= 0);
+
+
+/*
+5 cones sold...  // if there are enough cones
+
+Cannot sell you 6 cones I only have 3...  // If there are not enough cones
+
+Yay! I sold them all! // If there are no more cones*/
+
