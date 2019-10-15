53fcc10649cf25a369252a9d5c43f54afc4e40aa
Nico Virgino <nicovirgino@gmail.com>
2019-10-14 22:17:25 -0500

    loop exercise

---
+++ js/break_and_continue.js
@@ -0,0 +1,3 @@
+"use strict";
+
+prompt("Please input an odd number between 1 and 50: ");
---
+++ js/while.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//make a loop that multiplies by itself
+
+var i= 0;
+
+while (i>10) {
+    console.log('while loop iteration #' + i);
+}
