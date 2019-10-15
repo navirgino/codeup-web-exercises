4d118a9918fccca72e4721876a5b3b49091f0403
Nico Virgino <nicovirgino@gmail.com>
2019-10-15 09:13:51 -0500

    tuesday warmup

--- js/while.js
+++ js/while.js
@@ -2,8 +2,38 @@
 
 //make a loop that multiplies by itself
 
-var i= 0;
+/*var i = 0;
+while (i<16) {
+    console.log(i);
+}*/
+// var n = 2;
+// var x = 0;
+// while(n<16){
+//     console.log(n++);
+//     console.log(n *= x++);
+// }
 
-while (i>10) {
-    console.log('while loop iteration #' + i);
+var n = 0;
+var m = 0;
+while(n<17){
+    console.log(n++ * m++ * 2);
 }
+
+/*
+2
+4
+8
+16
+32
+64
+128
+256
+512
+1024
+2048
+4096
+8192
+16384
+32768
+65536
+*/
