3a01640e5a71a870483b9b9cdd7ea5c484a660c2
Nico Virgino <nicovirgino@gmail.com>
2019-10-16 14:08:47 -0500

    while for loops exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,27 +1,7 @@
 "use strict";
-//
 
 
-
-
-/*for(var userInput = prompt("Please input an odd number"); userInput>0; userInput--){
-    if(userInput%2 !==0){
-      break;
-    }
-    for(var b = 1; b > 50; b--) {
-        continue;
-        if (b % 2 === 0) {
-
-        } else {
-            console.log("Yikes! Skipping number: " + b);
-            console.log(b);
-        }
-    }
-}*/
-
-
-
-for (var a = 1; a < 50; a++) {
+/*for (var a = 1; a < 50; a++) {
     var userInput = prompt("Please input an odd number between 1 and 50: ");
     if (userInput % 2 !== 0) {
         break;
@@ -37,5 +17,26 @@ for (var a = 1; a < 50; a++) {
      }
 
      console.log(b);
- }
+ }*/
 
+
+ /*DAVID'S SOLUTION BELOWWWWWWWWWWWWWWWW*/
+
+/*
+while(true){
+     var num = parseInt(prompt("Please input an odd number between 1 and 50, stupid: "));
+     if(num%2 !== 0 && num < 50 && num >= 1){
+         break;
+     }
+
+}
+
+console.log("Skipping the number: " + num);
+
+for (var q = 1; q <= 50; q += 2){
+    if(q === num){
+        console.log("Jenkies, skipping that number, STOOPIT!!!!!");
+        continue;
+    }
+    console.log(q);
+}*/
