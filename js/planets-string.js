63bb9b262b263851182c20f3d991bc440c94625f
Nico Virgino <nicovirgino@gmail.com>
2019-10-17 15:59:43 -0500

    manipulating arrays exercise

--- js/planets-string.js
+++ js/planets-string.js
@@ -1,9 +1,8 @@
 (function(){
     "use strict";
 
-    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
-    var planetsArray = planetsString.split(', ');
-    console.log(planetsArray);
+
+
     /**
      * TODO:
      * Convert planetsString to an array, and save it in a variable named
@@ -11,17 +10,37 @@
      * console.log planetsArray to check your work
      */
 
-    var planets = "Mercury<br>Venus<br>Earth<br>Mars<br>Jupiter<br>Saturn<br>Uranus<br>Neptune";
-    console.log(planets);
+    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+    var planetsArray = planetsString.split("|");
+
+    console.log(planetsArray);
+
+
     /**
      * TODO:
      * Create a string with <br> tags between each planet. console.log() your
      * results. Why might this be useful?
-     *
-     * BONUS:
+     */
+
+    var namesString = planetsArray.join('<br>');
+    console.log(namesString);
+    document.write(namesString);
+     /** BONUS:
      * Create another string that would display your planets in an undordered
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
+
+
+
+     planetsArray.unshift('<ul>');
+     var planetsLi = planetsArray.join('<li>');
+     planetsArray.push('</ul>');
+
+
+     document.write(planetsLi);
+     console.log(planetsLi);
+
+
 
 })();
